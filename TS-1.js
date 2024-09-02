ada920c85eb0ceee10351c6d27962a4c229c413f
RayhanRK <m.rayhankhan01@gmail.com>
2024-09-02 19:14:07 +0600

    Added TS-1

--- TS-1.js
+++ TS-1.js
@@ -2,3 +2,38 @@
 //  If you Buy
 // - Burger more than 500tk: free Coke |
 // - Else Coke: 30tk
+
+// Method-1
+
+// let burger = 390;
+
+// if (burger > 500){
+//     console.log(coke = "free coke");
+// }
+// else{
+//     console.log( coke = "30tk");
+// }
+
+// method-2
+
+// let burgerPrice = 170;
+// let cokePrice = 30;
+
+// if( burgerPrice > 500 ){
+//     cokePrice = 0;
+//     console.log('Your coke is free');}
+
+//     else{
+//         console.log(cokePrice);
+//     }
+
+
+let burgerPrice = 170;
+
+if( burgerPrice > 500 ){
+    cokePrice = 0;
+    console.log('Your coke is free');}
+
+    else{
+        console.log(cokePrice = '30 taka');
+    }
