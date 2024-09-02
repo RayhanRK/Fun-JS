a351ba4e190a7f1a097ca1a6fefaff02cdb75f69
RayhanRK <m.rayhankhan01@gmail.com>
2024-09-02 21:04:23 +0600

    BMI calulator & health category added.

--- TS-2.js
+++ TS-2.js
@@ -8,6 +8,56 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - BMI < 18.5, you are underweight.
     - BMI >= 18.5 and BMI <=24.9, you are normal.
     - BMI >=25 and BMI <= 29.9, you are overweight.
-    - Otherwise, you are obese.
+    - Otherwise, You are obese.
 
 */
+
+// Method-1
+
+let weight = 77;
+let height = 1.72;
+let bmi = weight / height**2;
+let BMI = bmi.toFixed(2);
+console.log('Your BMI is: ' + BMI);
+
+if(BMI < 18.5){
+    console.log('You are underweight.')
+}
+
+else{
+    if( BMI >= 18.5 && BMI<= 24.9){
+        console.log('You are normal.')
+    }
+    else{
+        if(  BMI >=25 && BMI <= 29.9){
+            console.log('You are overweight.')
+        } else{
+            console.log('You are obese.')
+        }
+    }
+}
+
+
+// method-2 :
+
+let weight = 63;
+let height = 1.68;
+let bmi = weight / height**2;
+console.log('Your BMI is:' + bmi.toFixed(2));
+
+if(bmi < 18.5){
+    console.log('You are underweight.')
+}
+
+else{
+    if( bmi >= 18.5 && bmi <= 24.9){
+        console.log('You are normal.')
+    }
+    else{
+        if(bmi >=25 && bmi <= 29.9){
+            console.log('You are overweight.')
+        } else{
+            console.log('You are obese.')
+        }
+    }
+}
